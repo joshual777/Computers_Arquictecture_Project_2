519e5a947610137a37e2693bc83ea60994af54e5
Joshua <josui77@hotmail.com>
2022-05-08 11:59:55 -0600

    Adder test changes to run simulation

--- microarquitectura/ALU/sumador_nb_test.sv
+++ microarquitectura/ALU/sumador_nb_test.sv
@@ -1,33 +1,31 @@
 module sumador_nb_test();
-	
-		logic [N-1:0] a, b;
+		
+		logic [24:0] a, b;
 		logic ci;
-		logic [N-1:0] sum;
+		logic [24:0] sum;
 		logic co;
 		
-		sumador_nb sumador(a,b,ci,sum,co);
+		sumador_nb #(24)sumador(a,b,ci,sum,co);
 		
 		initial begin
 		
-		#20
+		#20;
 		ci = 0;
-		a = 4'b0011;
-		b = 4'b0001;
+		a = 24'b0011;
+		b = 24'b0001;
 		
-		#20
+		#20;
 		ci = 0;
-		a = 4'b0011;
-		b = 4'b0001;
+		a = 24'b0011;
+		b = 24'b0001;
 		
-		#20
+		#20;
 		ci = 0;
-		a = 4'b0011;
-		b = 4'b0001;
+		a = 24'b0011;
+		b = 24'b0001;
 		
-		
-		
-		end
+	   end
 
-end module
+endmodule
 
 
